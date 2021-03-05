f975096e1ceb476f7b27e7faee1e5bd484a35a65
insurecar <rostyslavshyian@gmail.com>
2021-03-05 12:13:50 +0200

    ssad

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up.js to startapp!</Text>
+      <Text>Open up.js to </Text>
       <StatusBar style="auto" />
     </View>
   );
